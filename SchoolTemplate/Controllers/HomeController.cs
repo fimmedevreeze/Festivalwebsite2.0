f56868d9616011cf566d840ddd018f50ee570338
Fimme de Vreeze <fimmedevreeze@outlook.com>
2020-03-02 11:55:13 +0100

    Indez overgezt van replit

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=110109;Uid=110109;Pwd=ww;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=110109;Uid=110109;Pwd=EGNARDeA;";
 
     public IActionResult Index()
     {
@@ -20,47 +20,57 @@ namespace SchoolTemplate.Controllers
       // products = GetProducts();
 
       return View(products);
-    }
-
-    private List<Product> GetProducts()
-    {
-      List<Product> products = new List<Product>();
-
-      using (MySqlConnection conn = new MySqlConnection(connectionString))
-      {
-        conn.Open();
-        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-        using (var reader = cmd.ExecuteReader())
-        {
-          while (reader.Read())
-          {
-            Product p = new Product
-            {
-              Id = Convert.ToInt32(reader["Id"]),
-              Naam = reader["Naam"].ToString(),
-              Calorieen = float.Parse(reader["calorieen"].ToString()),
-              Formaat = reader["Formaat"].ToString(),
-              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
-              Prijs = Decimal.Parse(reader["Prijs"].ToString())
-            };
-            products.Add(p);
-          }
-        }
-      }
-
-      return products;
-    }
+    }  
 
     public IActionResult Privacy()
     {
       return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+    [Route("festival/(id)")]
+    public IActionResult Festival(string id)
+    {
+        ViewData["id"] = id;
+
+        return View();
+    }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-  }
+
+
+    private List<Product> GetProducts()
+    {
+        List<Product> products = new List<Product>();
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product p = new Product
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Naam = reader["Naam"].ToString(),
+                        Calorieen = float.Parse(reader["calorieen"].ToString()),
+                        Formaat = reader["Formaat"].ToString(),
+                        Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+                        Prijs = Decimal.Parse(reader["Prijs"].ToString())
+                    };
+                    products.Add(p);
+                }
+            }
+        }
+
+        return products;
+    }
+    }
 }
